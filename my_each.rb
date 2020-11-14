d587d7f10c47b91fb7b32dec63cdb7c389cbf1f5
Charles <charlesangeloplai@gmail.com>
2020-11-13 19:50:15 -0500

    commit tree 4704bd27cb3622bea04d563f0b1a0c7482ccec31

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
+collection = []
+
+def my_each(collection)
   # code here
+  if block_given?
+    i = 0
+
+    while i < collection.length
+      yield(collection[i])
+      i += 1
+    end
+  end
+  
+  collection
+end
+
+my_each(collection) do |i|
+  return i
 end
